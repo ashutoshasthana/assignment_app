7f5c96ee5b8180c81c2687c052f1ff015b966d46
ashutoshasthana <aaashutoshasthana9@gmail.com>
2021-08-21 02:14:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component";
+import TodosList from "./components/todos-list.component";
+import CreateTodo from "./components/create-todo.component";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={TodosList} />
+        <Route path="/create" component={CreateTodo} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
